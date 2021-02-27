8d2ffe83582228217995407068f40790b2d264f9
Kamil Michalski <kamilko19@gmail.com>
2021-02-27 15:39:11 +0000

    make it better

--- main.js
+++ main.js
@@ -5,39 +5,37 @@ var users = {
 }
 socket.on('connection',function(client){
     console.log(`new connection ! ${client.id}`);
-    client.on("ghright",(x, y, velX, velY, turning)=>{
-            socket.emit("ghright", x, y, velX, velY, turning, client.id);
+    client.on("ghright",(x, y, velX, velY, turning, cid)=>{
+            client.broadcast.emit("ghright", x, y, velX, velY, turning, client.id);
             console.log("ghright");
     })
-    client.on("ghleft",(x, y, velX, velY, turning)=>{
-            socket.emit("ghleft", x, y, velX, velY, turning, client.id);
+    client.on("ghleft",(x, y, velX, velY, turning, cid)=>{
+            client.broadcast.emit("ghleft", x, y, velX, velY, turning, client.id);
             console.log("ghleft");
     })
-    client.on("ghup",(x, y, velX, velY, turning)=>{
-            socket.emit("ghup", x, y, velX, velY, turning, client.id);
+    client.on("ghup",(x, y, velX, velY, turning, cid)=>{
+            client.broadcast.emit("ghup", x, y, velX, velY, turning, client.id);
             console.log("ghup");
     })
-    client.on("ghdown",(x, y, velX, velY, turning)=>{
-            socket.emit("ghdown", x, y, velX, velY, turning, client.id);
+    client.on("ghdown",(x, y, velX, velY, turning, cid)=>{
+            client.broadcast.emit("ghdown", x, y, velX, velY, turning, client.id);
             console.log("pdown");
     })
-    client.on("pright",(x, y, velX, velY, turning)=>{
-            socket.emit("pright", x, y, velX, velY, turning, client.id);
+    client.on("pright",(x, y, velX, velY, turning, cid)=>{
+            client.broadcast.emit("pright", x, y, velX, velY, turning, client.id);
             console.log("pright");
     })
-    client.on("pleft",(x, y, velX, velY, turning)=>{
-            socket.emit("pleft",x, y, velX, velY, turning, client.id);
+    client.on("pleft",(x, y, velX, velY, turning, cid)=>{
+            client.broadcast.emit("pleft",x, y, velX, velY, turning, client.id);
             console.log("pleft");
     })
-    client.on("pup",(x, y, velX, velY, turning)=>{
-            socket.emit("pup", x, y, velX, velY, turning, client.id);
+    client.on("pup",(x, y, velX, velY, turning, cid)=>{
+            client.broadcast.emit("pup", x, y, velX, velY, turning, client.id);
             console.log("pup");
-
     })
-    client.on("pdown",(x, y, velX, velY, turning)=>{
-            socket.emit("pdown",x, y, velX, velY, turning, client.id);
+    client.on("pdown",(x, y, velX, velY, turning, cid)=>{
+            client.broadcast.emit("pdown",x, y, velX, velY, turning, client.id);
             console.log("pdown");
-            console.log(client.id);
     })
 });
 socket.listen(process.env.PORT || 2731)
