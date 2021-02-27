15005b1b90bee21c9627b014fb2a3c05815cb721
Kamil Michalski <kamilko19@gmail.com>
2021-02-27 14:52:16 +0000

    make it better

--- main.js
+++ main.js
@@ -6,36 +6,53 @@ var users = {
 socket.on('connection',function(client){
     console.log(`new connection ! ${client.id}`);
     client.on("ghright",(x, y, velX, velY, turning)=>{
+        if(client.id == client.id){
             socket.emit("ghright", x, y, velX, velY, turning);
             console.log("ghright");
+        }
     })
     client.on("ghleft",(x, y, velX, velY, turning)=>{
+        if(client.id == client.id){
             socket.emit("ghleft", x, y, velX, velY, turning);
             console.log("ghleft");
+        }
     })
     client.on("ghup",(x, y, velX, velY, turning)=>{
+        if(client.id == client.id){
             socket.emit("ghup", x, y, velX, velY, turning);
             console.log("ghup");
+        }
     })
     client.on("ghdown",(x, y, velX, velY, turning)=>{
+        if(client.id == client.id){
             socket.emit("ghdown", x, y, velX, velY, turning);
             console.log("pdown");
+        }
     })
     client.on("pright",(x, y, velX, velY, turning)=>{
+        if(client.id == client.id){
             socket.emit("pright", x, y, velX, velY, turning);
             console.log("pright");
+        }
     })
     client.on("pleft",(x, y, velX, velY, turning)=>{
+        if(client.id == client.id){
             socket.emit("pleft",x, y, velX, velY, turning);
             console.log("pleft");
+        }
     })
     client.on("pup",(x, y, velX, velY, turning)=>{
+        if(client.id == client.id){
             socket.emit("pup", x, y, velX, velY, turning);
             console.log("pup");
+        }
     })
     client.on("pdown",(x, y, velX, velY, turning)=>{
+        if(client.id == client.id){
             socket.emit("pdown",x, y, velX, velY, turning);
             console.log("pdown");
+            console.log(client.id);
+        }
     })
 });
 socket.listen(process.env.PORT || 2731)
